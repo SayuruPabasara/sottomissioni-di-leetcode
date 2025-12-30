bda43c12010ab576cbe55e239c92ee1f92ec6f1b
SayuruPabasara <sayurueragoda200254@gmail.com>
2025-12-31 01:14:55 +0530

    Time: 0 ms (100%), Space: 12.5 MB (94.74%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -1,15 +1,5 @@
 class Solution(object):
     def repeatedSubstringPattern(self, s):
-        counts=[]
-        
-        if s:
-            for i in s:
-                count=s.count(i)
-                counts.append(count)
-                k=len(counts)
-            counts_set=set(counts)
-            if len(counts_set)==1 and k>1:
-                return True
-        return False        
+        return s in (s+s)[1:-1]      
         
         
